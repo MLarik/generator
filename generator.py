989b64a9e3e2f8962c446c1426b5a678c712f822
MLarik <matew.larik@gmail.com>
2018-08-13 14:57:04 +0300

    second commit

--- generator.py
+++ generator.py
@@ -2,9 +2,16 @@ import yaml
 
 def generator(f_schema):
     cfg = yaml.load(open(f_schema))
-    for iter in range(len(cfg)):
-        cfg.keys(iter)
+    statements = []
+    for i in range(len(cfg)):
+        string = 'create table(\n'
+        current_key = cfg.keys(i)
+        string += current_key
+        fields = cfg.get(current_key).get('fields')
+        for j in range(len(fields)):
+            current_key2 = fields.keys(j)
+            string += '    ' + current_key2 + fields.get(current_key2) + '\n'
 
-
+        string += ');'
 
 generator('schema.yml')
